1cbe77ca6082009a9bd8d3efd3de951a0606baac
rxwyun <1120309488@qq.com>
2023-03-24 17:45:29 +0800

    兼容php8.2

--- src/Base/Request.php
+++ src/Base/Request.php
@@ -1,10 +1,12 @@
 <?php
 
 /**
- * Rangine Http Message
+ * WeEngine Api System
  *
  * (c) We7Team 2019 <https://www.w7.cc>
  *
+ * This is not a free software
+ * Using it under the license terms
  * visited https://www.w7.cc for more details
  */
 
@@ -20,6 +22,7 @@ use W7\Http\Message\Uri\Uri;
  * Class Request
  * @package W7\Http\Message\Base
  */
+#[\AllowDynamicProperties]
 abstract class Request implements RequestInterface {
 	use MessageTrait;
 
--- src/Base/Response.php
+++ src/Base/Response.php
@@ -1,10 +1,12 @@
 <?php
 
 /**
- * Rangine Http Message
+ * WeEngine Api System
  *
  * (c) We7Team 2019 <https://www.w7.cc>
  *
+ * This is not a free software
+ * Using it under the license terms
  * visited https://www.w7.cc for more details
  */
 
@@ -16,6 +18,7 @@ use Psr\Http\Message\ResponseInterface;
  * Class Response
  * @package W7\Http\Message\Base
  */
+#[\AllowDynamicProperties]
 abstract class Response implements ResponseInterface {
 	use MessageTrait;
 
